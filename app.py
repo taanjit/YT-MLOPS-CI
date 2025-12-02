e780714640a0dce74e71bd23f7039559429d1284
Dr Anjit <taanjit@gmail.com>
2025-12-02 04:13:49 +0000

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,10 @@ n = st.number_input("Enter an integer", value=1, step=1)
 square = n ** 2
 cube = n ** 3
 fifth_power = n ** 5
+tenth_power = n ** 10
 
 # Display results
-st.write(f"The square of {n} is: {square}")
-st.write(f"The cube of {n} is: {cube}")
-st.write(f"The fifth power of {n} is: {fifth_power}")
+st.write(f"The square of {n} is: {square} \n")
+st.write(f"The cube of {n} is: {cube} \n")
+st.write(f"The fifth power of {n} is: {fifth_power} \n")
+st.write(f"The tenth power of {n} is: {tenth_power} \n")
